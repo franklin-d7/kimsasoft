5ed6eac184561eb7fcd87cbcf619870888537490
Franklin Rochina <frochina.1999@gmail.com>
2023-01-08 10:25:19 -0500

    Install container mongo and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB)
   ],
   controllers: [AppController],
   providers: [AppService],
